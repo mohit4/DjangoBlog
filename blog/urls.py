0fe75bf96470554574a09b03d2ca6ccf99115343
mohit kumar (KUMARMOH) <mohit.kumar3@amdocs.com>
2018-07-30 23:59:42 +0530

    Added post : create, list and detail view

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,12 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+
     url(r'^$', views.IndexView.as_view(), name='index'),
+
+    url(r'^create_post/$', views.PostCreateView.as_view(), name='create_post'),
+
+    url(r'^list_posts/$', views.PostListView.as_view(), name='list_posts'),
+
+    url(r'^post_details/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='post_details'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,50 @@ from __future__ import unicode_literals
 
 # django core
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
+
+# user defined
+from .models import Post
 
 class IndexView(TemplateView):
     '''
     For homepage
     '''
     template_name = 'blog/index.html'
+
+#############################################
+# Model CRUD Views
+#############################################
+
+class PostCreateView(CreateView):
+    '''
+    For creating new post
+    '''
+    fields = ('title','text')
+    model  = Post
+    template_name = 'blog/post_create.html'
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreateView, self).form_valid(form)
+
+#############################################
+
+class PostListView(ListView):
+    '''
+    For listing all posts
+    '''
+    context_object_name = 'posts'
+    model = Post
+    template_name = 'blog/post_list.html'
+
+#############################################
+
+class PostDetailView(DetailView):
+    '''
+    For displaying detailed view of the post
+    '''
+    model = Post
+    template_name = "blog/post_detail.html"
